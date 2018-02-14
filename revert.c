a24bafb5e6ba5a4a63833b8aab5b6386108bb618
Del_Uesr <Del_Uesr@gmail.com>
2018-02-14 08:27:51 +0000

    Changed revert.c in br1 of workspace1

--- revert.c
+++ revert.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
-#include <string.h>
- 
-int main()
-{
-   char arr[100];
- 
-   printf("Enter a string to reverse\n");
-   gets(arr);
- 
-   strrev(arr);
- 
-   printf("Reverse of the string is \n%s\n", arr);
- 
-   return 0;
-}
+
+int main() {
+    int n, n1, rev = 0, rem;
+    
+    printf("Enter any number: ");
+    scanf("%d", &n);    
+    n1 = n;
+    
+    /* logic */    while (n > 0){
+        rem = n % 10;
+        rev = rev * 10 + rem;
+        n = n / 10;
+    }
+    
+    if (n1 == rev){
+        printf("Given number is a palindromic number"); 
+    }
+    else{
+        printf("Given number is not a palindromic number"); 
+    }    
+    return 0; 
+} 
